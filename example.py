0d282d0a5598d8357cfc0a1844b45d198bdf97d5
haretaro <frontend.1000@gmail.com>
2016-11-04 22:09:17 +0900

    少し変えただけ

--- example.py
+++ example.py
@@ -3,5 +3,6 @@ from twihoge import TwitterApi
 #トレンドワードから1つ選んで検索し,結果を表示するデモ
 twitter = TwitterApi.load_json_file('keys.json')
 trend = twitter.trendwords()[0]
-for statustext in twitter.search_statustexts(trend):
+print('trend word = {}'.format(trend))
+for statustext in twitter.search_statustexts(trend, count=20):
     print(statustext)
